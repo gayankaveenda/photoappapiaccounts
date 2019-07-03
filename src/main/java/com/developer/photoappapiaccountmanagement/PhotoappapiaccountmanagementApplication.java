42b8835e6981fb8977bc6d34fea9fcff4a991b56
Gayan kaveenda <gayankaveenda@gmail.com>
2019-07-03 16:37:09 +1000

    Accounts management project Initialized

---
+++ src/main/java/com/developer/photoappapiaccountmanagement/PhotoappapiaccountmanagementApplication.java
@@ -0,0 +1,15 @@
+package com.developer.photoappapiaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PhotoappapiaccountmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappapiaccountmanagementApplication.class, args);
+    }
+
+}
